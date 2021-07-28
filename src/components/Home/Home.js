2022d370311e36e0f1536858f8cd2388ab54d213
Jack Rigan <riganjack40@gmail.com>
2021-07-27 20:35:06 -0600

    initial commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {BrowserRouter as Router, Link,Switch} from 'react-router-dom';
+import Bg from '../../assets/Images/home-bg.jpeg'
+
+
+function Home() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Home
